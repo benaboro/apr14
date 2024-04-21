ae6439a0a29090cffbfd7f3d1adfc581e053366b
benaboro <167253652+benaboro@users.noreply.github.com>
2024-04-20 23:38:03 -0500

    customer

---
+++ Customer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mybank;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shouvik
+ */
+public class Customer {
+     private int id;
+    private String name;
+    private String phone;
+    private CustomerType custType;
+   ArrayList<Account> accounts;
+
+}
